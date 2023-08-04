7378c2ab617d9e20991c58d8e4339cbf89325f65
FayyadArsy <fayyadarsynandi2@gmail.com>
2023-08-05 00:16:50 +0700

    fix bug

--- js/script.js
+++ js/script.js
@@ -22,4 +22,8 @@ const sr = ScrollReveal({
 });
 
 sr.reveal('.hero-text', { origin: 'top' });
-
+sr.reveal('.about-img, .service-item, .about-text', { origin: 'bottom' });
+sr.reveal('.about-text h2, .text-center, .right-contact h2', { origin: 'top' });
+sr.reveal('.left-contact', { origin: 'left' });
+sr.reveal('.right-contact', { origin: 'left' });
+sr.reveal('.end-section', { origin: 'top' });
